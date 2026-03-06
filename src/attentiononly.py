b10572b5f6501454fc5206d5c5e5b1f96a60a024
xiangze <xiangze@gmail.com>
2026-03-06 23:20:56 +0900

    add embedding mode(default False)

--- src/attentiononly.py
+++ src/attentiononly.py
@@ -9,8 +9,7 @@ class AttentionOnlyBlock(nn.Module):
         super().__init__()
         d_model=params["d_model"]
         n_heads=params["nhead"]
-        #max_len= params["max_len"]
-        #pad_id=params["pad_id"]
+        #max_len= params["max_len"]  #pad_id=params["pad_id"]
         dropout=params["dropout"]
 
         self.self_attn = nn.MultiheadAttention(
@@ -62,14 +61,19 @@ class SharedAttentionOnly(nn.Module):
         self.step_embed = None
         self.weightvisible=weightvisible
         self.debug=debug
+        self.B=params["batch_size"]
+        self.L=params["seq_len"]
 
     def forward(self,
         x_tok: torch.Tensor,                 # (B, L, d_model) すでにトークン側で埋め込み済み
         attn_mask: torch.Tensor = None,         # (L, L) など（必要な場合）
         key_padding_mask: torch.Tensor = None,  # (B, L) 1=keep/0=pad なら (==0) を渡す
     ) -> torch.Tensor:
-        B, L,d_model  = x_tok.shape
-        pos_ids = torch.arange(L, device=x_tok.device, dtype=x_tok.dtype).unsqueeze(0).expand(B, L)
+        #B, L,d_model  = x_tok.shape
+        if(self.debug):
+            print("x",x_tok.shape)
+            print("B,L",self.B, self.L)
+        pos_ids = torch.arange(self.L, device=x_tok.device, dtype=x_tok.dtype).unsqueeze(0).expand(self.B, self.L)
         h = x_tok + pos_ids.unsqueeze(-1)  # broadcast over d_model
 
         if(self.debug):
@@ -77,7 +81,7 @@ class SharedAttentionOnly(nn.Module):
             print("attn_mask",attn_mask)
         for t in range(self.steps):
             if self.step_embed is not None:
-                step_ids = torch.full((B, L), t, dtype=torch.long, device=x_tok.device)
+                step_ids = torch.full((self.B, self.L), t, dtype=torch.long, device=x_tok.device)
                 h = h + self.step_embed(step_ids)
             attn_out ,_ = self.attn(
                 h,h,h,
@@ -97,6 +101,8 @@ class AttentionOnlyNet(nn.Module):
         num_layers=params["num_layer"]
         self.pad_id=params["pad_id"]
         self.debug=debug
+        self.betch_size=params["batch_size"]
+        self.seq_len=params["seq_len"]
         if(recursive):
             self.layers = nn.ModuleList([SharedAttentionOnly(params,weightvisible=weightvisible)])
         else:
@@ -106,6 +112,7 @@ class AttentionOnlyNet(nn.Module):
             self.tok = nn.Embedding(params["vocab_size"], params["d_model"])
         else:
             self.tok = None 
+
         self.embedding=embedding
     def forward(self,
         ids: torch.Tensor,
@@ -113,17 +120,20 @@ class AttentionOnlyNet(nn.Module):
         key_padding_mask: torch.Tensor | None = None,
         src_ids: torch.Tensor | None = None,   # ← token IDを別引数で受け取る
     ) -> torch.Tensor:
+        
         if(self.embedding):
             ids = self.tok(ids)
+            ids=self.tok(ids)
         else:
             ids=ids.to(torch.float32)
+            attn_mask=attn_mask.to(torch.float32)
+
         if key_padding_mask is None:# 2D: (batch, seq_len)
             if src_ids is not None:
                 key_padding_mask = (src_ids == self.pad_id)  
                 #key_padding_mask = (ids == self.pad_id)
             elif attn_mask is not None:
-                B,S=params["batch_size"],params["seq_len"]
-                key_padding_mask=torch.tensor(attn_mask[0,:].repeat(B).reshape((B,S)),dtype=torch.bool)
+                key_padding_mask=torch.tensor(attn_mask[0,:].repeat(self.betch_size).reshape((self.betch_size,self.seq_len)),dtype=torch.bool)
                 key_padding_mask=~key_padding_mask
                 #key_padding_mask = (attn_mask == 0) # True=padding
             else:
@@ -148,28 +158,28 @@ class AttentionOnlyNet(nn.Module):
         return ids
 
 class AttentionOnlyRegressor(AttentionOnlyNet):
-    def __init__(self, params:dict,debug=False,recursive=False,weightvisible=False):
-        super().__init__(params,debug,recursive,weightvisible)
+    def __init__(self, params:dict,debug=False,recursive=False,weightvisible=False,embedding=False):
+        super().__init__(params,debug,recursive,weightvisible,embedding)
         d_model=params["d_model"]
         self.head = nn.Sequential(
             nn.LayerNorm(d_model),
             nn.Linear(d_model, 1)
         )
-
     def forward(self, x: torch.Tensor, attn_mask: torch.Tensor | None = None, key_padding_mask: torch.Tensor | None = None) -> torch.Tensor:
         cls = super().forward(x, attn_mask=attn_mask, key_padding_mask=key_padding_mask)[:, 0, :]  # (B, d_model)
         yhat = self.head(cls)  # (B,1)
         return yhat
 
 class AttentionOnlyRecursiveRegressor(AttentionOnlyRegressor):
-    def __init__(self, params:dict,debug=False,weightvisible=False):
-        super().__init__(params,debug,True,weightvisible)
+    def __init__(self, params:dict,debug=False,weightvisible=False,embedding=False):
+        super().__init__(params,debug,True,weightvisible,embedding)
 
 if __name__ == "__main__":
     import argparse    
     parser = argparse.ArgumentParser(description="masked attention, recursive")
     parser.add_argument("--debug", action="store_true")    
     parser.add_argument("--key_padding_none", action="store_true")    
+    parser.add_argument("--with_embedding", action="store_true")    
     args = parser.parse_args()
 
     params={
@@ -182,7 +192,6 @@ if __name__ == "__main__":
         "pad_id":0,
     }
     params["debug"]=args.debug
-
     #no emmbedding
     ids=torch.randint(0,10,(params["batch_size"], params["seq_len"], params["d_model"]) ) # すでに埋め込み済みの入力　内部でfloatにする
     mask= torch.tensor([ [0]*(params["seq_len"]-5) +[1]*5]* params["seq_len"],dtype=torch.bool)
@@ -194,6 +203,6 @@ if __name__ == "__main__":
     for k,v in d.items():
         print(k)
         net = v(params,debug=params["debug"])        
-        out = net(ids,mask, key_padding_mask)
+        out = net(ids,mask, key_padding_mask,embedding=params["embedding"])
         print(out)
 
